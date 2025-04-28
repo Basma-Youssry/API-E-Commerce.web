e4ae7aad9428bf1d5bde9bf9515682bffd0347fa
7777777 <7777777@DESKTOP-FHSN7EE>
2025-04-28 06:17:30 +0300

    07.Search Specification

--- Core/Service/Specifications/ProductWithBrandAndTypeSpecifications.cs
+++ Core/Service/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -13,8 +13,9 @@ namespace Service.Specifications
         //Get All Products with types and Brands
 
         public ProductWithBrandAndTypeSpecifications(ProductQueryParams queryParams)
-            :base(P => (!queryParams.BrandId.HasValue || P.BrandId == queryParams.BrandId)
-            &&    (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId))
+            : base(P => (!queryParams.BrandId.HasValue || P.BrandId == queryParams.BrandId)
+            && (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId)
+            && (string.IsNullOrWhiteSpace(queryParams.SearchValue) || P.Name.ToLower().Contains(queryParams.SearchValue.ToLower())))
         {
             AddInclude(P => P.ProductBrand);
             AddInclude(P => P.ProductType);
