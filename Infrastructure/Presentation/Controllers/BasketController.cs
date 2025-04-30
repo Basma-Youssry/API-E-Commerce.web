cb8269c197136a57340cfcea5b343a55b120c83f
7777777 <7777777@DESKTOP-FHSN7EE>
2025-04-30 13:42:05 +0300

    13.Basket Module - Controller

---
+++ Infrastructure/Presentation/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransfareObjects.BasketModuleDTos;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BasketController(IServiceManager _serviceManager) : ControllerBase
+    {
+        //Get Basket
+        [HttpGet] //GET BaseUrl/api/Basket
+        public async Task<ActionResult<BasketDTo>> GetBasket(string key)
+        {
+            var Basket = await _serviceManager.BasketService.GetBasketAsync(key);
+            return Ok(Basket);
+        }
+
+        //Create Or Update Basket
+        [HttpPost]
+        public async Task<ActionResult<BasketDTo>> CreateOrUpdateBasket(BasketDTo basket)
+        {
+            var Basket = await _serviceManager.BasketService.CreateOrUpdateBasketAsync(basket);
+
+            return Ok(Basket);
+        }
+
+        //Delete Basket
+        [HttpDelete("{key}")] //DELETE BaseUrl/api/Basket/hdhhdjhjdh(Guid)
+        public async Task<ActionResult<bool>> DeleteBasket(string key)
+        {
+            var Result = await _serviceManager.BasketService.DeleteBasketAsync(key);
+            return Ok(Result);
+        }
+
+
+    }
+}
