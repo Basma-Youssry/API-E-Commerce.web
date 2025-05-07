c1d563e5d07809a9b5e8f6e3eec1251fd0d83e46
7777777 <7777777@DESKTOP-FHSN7EE>
2025-05-07 10:50:29 +0300

    02.Order Module Entities

---
+++ Core/DomainLayer/Models/OrderModule/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.OrderModule
+{
+    public class Order
+    {
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderAddress Address { get; set; } = default!;
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+        public int DeliveryMethodId { get; set; } //FK
+        public OrderStatus OrderStatus { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Price;
+    }
+}
