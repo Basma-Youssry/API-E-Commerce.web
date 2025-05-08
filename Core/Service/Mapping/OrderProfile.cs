efaea791c0ebc84050605d75b36a05fcae1a0b91
7777777 <7777777@DESKTOP-FHSN7EE>
2025-05-08 18:06:19 +0300

    05.Order Profile[Mapping]

--- Core/Service/Mapping/OrderProfile.cs
+++ Core/Service/Mapping/OrderProfile.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DomainLayer.Models.OrderModule;
 using Shared.DataTransfareObjects.IdentityDTO_s;
+using Shared.DataTransfareObjects.OrderDTOs;
 
 namespace Service.Mapping
 {
@@ -13,7 +14,14 @@ namespace Service.Mapping
     {
         public OrderProfile()
         {
-            CreateMap<AddressDTo, OrderAddress>();
+            CreateMap<AddressDTo, OrderAddress>().ReverseMap();
+
+            CreateMap<Order, OrderToReturnDTo>()
+                .ForMember(D => D.DeliveryMethod, O => O.MapFrom(S => S.DeliveryMethod.ShortName));
+
+            CreateMap<OrderItem, OrderItemDTo>()
+                        .ForMember(D => D.ProductName, O => O.MapFrom(S => S.Product.ProductName))
+                        .ForMember(D => D.PictureUrl, O => O.MapFrom<OrderItemPictureUrlResolver>());
         }
     }
 }
