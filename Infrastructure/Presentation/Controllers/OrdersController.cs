6a7672b84e240b5424b00153427555ba3ee67fa5
7777777 <7777777@DESKTOP-FHSN7EE>
2025-05-09 11:52:23 +0300

    07.Order Controller[Ge...rders - Get Order By Id]

--- Infrastructure/Presentation/Controllers/OrdersController.cs
+++ Infrastructure/Presentation/Controllers/OrdersController.cs
@@ -11,22 +11,46 @@ using Shared.DataTransfareObjects.OrderDTOs;
 
 namespace Presentation.Controllers
 {
+    [Authorize]
     public class OrdersController(IServiceManager _serviceManager) : APIBaseController
     {
         //Create Order
-        [Authorize]
+        
         [HttpPost]
-        public async Task<ActionResult<OrderToReturnDTo>> CreateOrder(OrderDTo orderDTo)
+        public async Task<ActionResult<OrderToReturnDTo>> CreateOrderAsync(OrderDTo orderDTo)
         {
            
-            var Order = await _serviceManager.OrderService.CreateOrder(orderDTo, GetEmailFromToken());
+            var Order = await _serviceManager.OrderService.CreateOrderAsync(orderDTo, GetEmailFromToken());
 
             return Ok(Order);
         }
 
         //Get Delivery Methods
+        [AllowAnonymous]
+        [HttpGet("DeliveryMethods")] // Get BaseUrl/api/Orders/DeliveryMethods
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDTo>>> GetDeliveryMethods()
+        {
+            var DeliveryMethods = await _serviceManager.OrderService.GetDeliveryMethodsAsync();
+            return Ok(DeliveryMethods);
+        }
 
-        //[HttpGet("DeliveryMethods")] //Get/api/Orders/DeliveryMethods
-        //public async Task<ActionResult<IEnumerable<DeliveryMethodDTo>>>
+        //Get All Order By Email
+
+       
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDTo>>> GetAllOrders()
+        {
+            var Order = await _serviceManager.OrderService.GetAllOrdersAsync(GetEmailFromToken());
+            return Ok(Order);
+        }
+
+        //Get Order By Id
+        
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<OrderToReturnDTo>> GetOrderById(Guid id)
+        {
+            var Order = await _serviceManager.OrderService.GetOrderByIdAsync(id);
+            return Ok(Order);
+        }
     }
 }
